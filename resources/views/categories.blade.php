b82d4f178c21689af396d9b04b440b2e52853fe0
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2015-06-24 00:49:03 -0300

    Views de Categorias e Produtos

---
+++ resources/views/categories.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arthur
+ * Date: 6/23/15
+ * Time: 11:03 AM
+ */
---
+++ resources/views/products.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: arthur
+ * Date: 6/24/15
+ * Time: 12:28 AM
+ */
