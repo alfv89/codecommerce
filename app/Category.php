ef15c47a20f99a6e1b129fc1c2e2d663b7239d14
Arthur Vasconcelos <vasconcelos.arthur@gmail.com>
2015-06-24 23:09:58 -0300

    Declaração de propriedades do model

--- app/Category.php
+++ app/Category.php
@@ -4,6 +4,10 @@ namespace CodeCommerce;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property mixed name
+ * @property mixed description
+ */
 class Category extends Model
 {
     protected $fillable = ['name', 'description'];
